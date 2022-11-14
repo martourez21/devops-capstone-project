f413cd6b385879b8b1fc055dcb9f1be8ecd1e5a9
martourez21 <nestorabiawuh@gmail.com>
2022-11-15 00:49:53 +0100

    completed ci build -01

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def get_account_list():
     """List all AccountsThis endpoint will list all Accounts"""
@@ -96,6 +97,7 @@ def get_account_by_id(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     """
@@ -105,7 +107,7 @@ def update_account(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")       
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
